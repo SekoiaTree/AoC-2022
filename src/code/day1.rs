838c2bb7442b4d4b15aae39295a6d5f97aa39fb6
SekoiaTree <sequoia.1009@gmail.com>
2022-12-01 16:07:40 +0100

    Optimization :tm:

--- src/code/day1.rs
+++ src/code/day1.rs
@@ -3,17 +3,48 @@ use std::time::Instant;
 type Data = Vec<Vec<u32>>;
 
 pub(crate) fn run(data : Data) -> u32 {
-    data.iter().map(|x| x.iter().sum()).max().unwrap()
+    let iterator = data.iter().map(|x| x.iter().sum());
+    let mut max = 0;
+    for i in iterator {
+        if i > max {
+            max = i;
+        }
+    }
+
+    max
 }
 
 #[cfg(feature = "part-two")]
 pub(crate) fn run_step2(data: Data) -> u32 {
+    /* This is O(nlog n). Cringe.
     let mut sorted : Vec<u32> = data.iter().map(|x| x.iter().sum()).collect();
     sorted.sort_by(|x,y| Ord::cmp(y, x));
 
-    sorted[..3].iter().sum()
+    sorted[..3].iter().sum()*/
+    // This is O(n).
+    let iterator = data.iter().map(|x| x.iter().sum());
+
+    let mut max = [0; 3];
+    for i in iterator {
+        if i > max[0] {
+            max[0] = i;
+            // A call to sort? Bad, but tbh max is so small it doesn't matter.
+            max.sort();
+        }
+    }
+    max.iter().sum()
+    // This is also O(n), but slower because it does 3 passes.
+    /*let mut sparse_sums : Vec<Option<u32>> =  data.iter().map(|x| Some(x.iter().sum())).collect();
+
+    let mut sum = 0;
+    for i in 0..3 {
+        let (index, value) = sparse_sums.iter().enumerate().max_by_key(|&(_, item)| *item).unwrap();
+        sum += value.unwrap();
+        sparse_sums[index] = None;
+    }
+    sum*/
 }
 
-pub(crate) fn convert(data: Vec<String>, _profiling: Instant) -> Data {
+pub(crate) fn convert(data: Vec<&str>, _profiling: Instant) -> Data {
     data.split(|x| x.is_empty()).map(|x| x.iter().map(|y| y.parse::<u32>().unwrap()).collect()).collect()
 }
--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,18 @@ fn main() {
     let profiling = Instant::now();
     println!("Today is day #{}. Beginning program...", DAY);
 
-    let contents: Vec<String> = INPUT.trim()
-        .split::<&str>("\n").map(|x| x.to_string()).collect();
-
+    let contents: Vec<&str> = INPUT.trim().lines().collect();
 
     let processed = day::convert(contents, profiling);
+    let processed_2 = processed.clone();
+
     let processing_time = profiling.elapsed();
     println!("Processing data complete... Time taken: {} microseconds or {} ms. \n", processing_time.as_micros(), processing_time.as_millis());
-    let result = day::run(processed.clone());
+    let result = day::run(processed);
     let time_taken_p1 = profiling.elapsed() - processing_time;
     println!("Result of part 1: {:?}. This took: {} microseconds or {} ms.", result, time_taken_p1.as_micros(), time_taken_p1.as_millis());
     #[cfg(feature = "part-two")]{
-        let result2 = day::run_step2(processed);
+        let result2 = day::run_step2(processed_2);
         let p2_time = profiling.elapsed() - time_taken_p1 - processing_time;
         println!("Result of part 2: {:?}. This took {} microseconds or {} ms.\n", result2, p2_time.as_micros(), p2_time.as_millis());
         println!("Total time taken: {} microseconds or {} ms.", (p2_time + time_taken_p1 + processing_time).as_micros(), (p2_time + time_taken_p1 + processing_time).as_millis());
