b1667503baf371809a2e68d01348995d93a1c379
SekoiaTree <sequoia.1009@gmail.com>
2022-12-12 23:08:20 +0100

    Optimize day 12

--- solves/src/code/day12.rs
+++ solves/src/code/day12.rs
@@ -1,8 +1,7 @@
 use std::cmp::Reverse;
-use std::collections::HashSet;
+use std::collections::{HashSet, VecDeque};
 use std::time::Instant;
 use priority_queue::PriorityQueue;
-use aoc_util::parse_in_grid;
 
 type Data = (Vec<Vec<u32>>, (usize, usize), (usize, usize));
 
@@ -10,31 +9,31 @@ pub fn run(data: Data) -> u32 {
     let (map, start, end) = data;
     let mut queue = PriorityQueue::new();
     let mut visited_set = HashSet::new();
-    queue.push(start, Reverse(0));
+    queue.push((start, 0), Reverse(0));
     while queue.len() > 0 {
-        let ((i, j), len) = queue.pop().unwrap();
+        let (((i, j), len), _priority) = queue.pop().unwrap();
         if !visited_set.insert((i, j)) {
             continue;
         }
 
         if (i, j) == end {
-            return len.0;
+            return len;
         }
 
-        if i > 0 && map[i-1][j] <= map[i][j]+1 {
-            queue.push((i-1, j), Reverse(len.0+1));
+        if i > 0 && map[i-1][j] <= map[i][j]+1 && !visited_set.contains(&(i-1, j)) {
+            queue.push(((i-1, j), len+1), Reverse(len as usize+1+end.0.abs_diff(i-1)+end.1.abs_diff(j)));
         }
 
-        if i < map.len()-1 && map[i+1][j] <= map[i][j]+1 {
-            queue.push((i+1, j), Reverse(len.0+1));
+        if i < map.len()-1 && map[i+1][j] <= map[i][j]+1 && !visited_set.contains(&(i+1, j)) {
+            queue.push(((i+1, j), len+1), Reverse(len as usize+1+end.0.abs_diff(i+1)+end.1.abs_diff(j)));
         }
 
-        if j > 0 && map[i][j-1] <= map[i][j]+1 {
-            queue.push((i, j-1), Reverse(len.0+1));
+        if j > 0 && map[i][j-1] <= map[i][j]+1 && !visited_set.contains(&(i, j-1)) {
+            queue.push(((i, j-1), len+1), Reverse(len as usize+1+end.0.abs_diff(i)+end.1.abs_diff(j-1)));
         }
 
-        if j < map[i].len()-1 && map[i][j+1] <= map[i][j]+1 {
-            queue.push((i, j+1), Reverse(len.0+1));
+        if j < map[i].len()-1 && map[i][j+1] <= map[i][j]+1 && !visited_set.contains(&(i, j+1)) {
+            queue.push(((i, j+1), len+1), Reverse(len as usize+1+end.0.abs_diff(i)+end.1.abs_diff(j+1)));
         }
     }
     panic!("No solution found!")
@@ -43,33 +42,33 @@ pub fn run(data: Data) -> u32 {
 #[cfg(feature = "part-two")]
 pub fn run_step2(data: Data) -> usize {
     let (map, _start, end) = data;
-    let mut queue = PriorityQueue::new();
+    let mut queue = VecDeque::new();
     let mut visited_set = HashSet::new();
-    queue.push(end, Reverse(0));
+    queue.push_back((end, 0));
     while queue.len() > 0 {
-        let ((i, j), len) = queue.pop().unwrap();
+        let ((i, j), len) = queue.pop_front().unwrap();
         if !visited_set.insert((i, j)) {
             continue;
         }
 
         if map[i][j] == 0 {
-            return len.0;
+            return len;
         }
 
-        if i > 0 && map[i][j] <= map[i-1][j]+1 {
-            queue.push((i-1, j), Reverse(len.0+1));
+        if i > 0 && map[i][j] <= map[i-1][j]+1 && !visited_set.contains(&(i-1, j)) {
+            queue.push_back(((i-1, j), len+1));
         }
 
-        if i < map.len()-1 && map[i][j] <= map[i+1][j]+1 {
-            queue.push((i+1, j), Reverse(len.0+1));
+        if i < map.len()-1 && map[i][j] <= map[i+1][j]+1 && !visited_set.contains(&(i+1, j)) {
+            queue.push_back(((i+1, j), len+1));
         }
 
-        if j > 0 && map[i][j] <= map[i][j-1]+1 {
-            queue.push((i, j-1), Reverse(len.0+1));
+        if j > 0 && map[i][j] <= map[i][j-1]+1 && !visited_set.contains(&(i, j-1)) {
+            queue.push_back(((i, j-1), len+1));
         }
 
-        if j < map[i].len()-1 && map[i][j] <= map[i][j+1]+1 {
-            queue.push((i, j+1), Reverse(len.0+1));
+        if j < map[i].len()-1 && map[i][j] <= map[i][j+1]+1 && !visited_set.contains(&(i, j+1)) {
+            queue.push_back(((i, j+1), len+1));
         }
     }
     panic!("No solution found!")
